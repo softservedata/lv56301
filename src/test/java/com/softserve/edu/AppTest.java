d3c6b250b00c7aa5900a59524158cb21e31602dd
soft <hahaha@gmail.com>
2021-08-04 13:58:03 +0300

    Example

---
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -0,0 +1,13 @@
+package com.softserve.edu;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void checkCreateFromRowInvalidRow() {
+        // Code
+        // TODO
+        System.out.println("done test");
+    }
+}
